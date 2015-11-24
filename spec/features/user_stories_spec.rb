50efd078706813039bf0481703a6a9870ef0fb2a
Alex Person <alexander.person@hyperisland.se>
2015-11-24 12:54:00 +0000

    (WIP) refactor step of previous commit

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,36 +1,36 @@
 describe "User Stories" do
-	# As an air traffic controller
-	# So planes can land safely at my airport
-	# I would like to instruct a plane to land 
-	it 'so planes can land at airports, instruct a plane to land' do
-		airport = Airport.new(20)
-		plane = Plane.new
-		allow(airport).to receive(:stormy?).and_return false
-		expect { airport.land(plane) }.not_to raise_error
-	end
-
-	# As an air traffic controller
-	# So planes can take off safely from my airport
-	# I would like to instruct a plane to take off
-	it 'so planes take off from airports, instruct a plane to take off' do
-		airport = Airport.new(20)
-		plane = Plane.new
-		airport.take_off(plane)
-		expect { airport.take_off(plane) }.not_to raise_error
-	end
-
-	# As an air traffic controller
-	# So that I can avoid collisions
-	# I want to prevent airplanes landing when my airport is full
-	context 'when airport is full' do
-		it 'does not allow planes to land' do
-			airport = Airport.new(20)
-			plane = Plane.new
+	let(:airport) { Airport.new(20) }
+	let(:plane) { Plane.new }
+	
+	context 'when not stormy' do
+		before do
 			allow(airport).to receive(:stormy?).and_return false
-			20.times do
-				airport.land(plane)
+		end
+		# As an air traffic controller
+		# So planes can land safely at my airport
+		# I would like to instruct a plane to land 
+		it 'so planes can land at airports, instruct a plane to land' do
+			expect { airport.land(plane) }.not_to raise_error
+		end
+
+		# As an air traffic controller
+		# So planes can take off safely from my airport
+		# I would like to instruct a plane to take off
+		it 'so planes take off from airports, instruct a plane to take off' do
+			airport.take_off(plane)
+			expect { airport.take_off(plane) }.not_to raise_error
+		end
+
+		# As an air traffic controller
+		# So that I can avoid collisions
+		# I want to prevent airplanes landing when my airport is full
+		context 'when airport is full' do
+			it 'does not allow planes to land' do
+				20.times do
+					airport.land(plane)
+				end
+				expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
 			end
-			expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
 		end
 	end
 
@@ -38,10 +38,10 @@ describe "User Stories" do
 	# So that I can avoid accidents
 	# I want to prevent airplanes landing or taking off when the weather is stormy
 	context 'when weather is stormy' do
-		it 'does not allow planes to land' do
-			airport = Airport.new(20)
-			plane = Plane.new
+		before do
 			allow(airport).to receive(:stormy?).and_return true
+		end
+		it 'does not allow planes to land' do
 			expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
 		end
 	end
